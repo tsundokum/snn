64d1aa4d4bff8ac177cf7ed0950d2ed8495292e2
Marat Zaynutdinov <tsundokum@gmail.com>
2013-03-31 18:50:48 +0400

    finished with gui + added russian words

--- distance-gui.py
+++ distance-gui.py
@@ -2,10 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import *
+import tkMessageBox
+import tkFont
 from createpropositions import read_all_propositions
+import random
 
+BUTTON_NEXT_TEXT = 'Дальше'
+MAIN_WINDOW_TITLE = " "
+LABEL_DEFAULT_TEXT = " "
+BUTTON_START_TEXT = "Начинаем!"
+FINISH_MESSAGE = "Эксперимент окончен. Спасибо!"
 
-proposition_words_file = 'resources/RogersMcClelland08.yaml'
+random.seed(113)
+
+proposition_words_file = 'resources/RogersMcClelland08_ru.yaml'
+output_file = 'resources/distances.csv'
+SCALER_DEFAULT_VALUE = 0
 
 
 def get_list_from_file(file_name):
@@ -19,34 +31,78 @@ def get_list_from_file(file_name):
     return rc
 
 
-def generate_stimuli():
-    return read_all_propositions(proposition_words_file)
+def generate_and_shuffle_propositions_generator():
+    props = read_all_propositions(proposition_words_file)
+    random.shuffle(props)
+
+    for i, prop in enumerate(props):
+        yield prop
+        if i >= 1:
+            break
+
+
+def proposition_text_view(proposition_as_tuple):
+    words_to_show = filter(lambda x: '$' not in x, proposition_as_tuple)
+    return ' '.join(words_to_show)
+
+
+def write_out_answer(proposition, scale_value):
+    with open(output_file, "a") as f:
+        write_line = u','.join(proposition + (unicode(scale_value),)) + u'\n'
+        print write_line,
+        f.write(write_line.encode('utf8'))
+
 
 
 class ScalerApp:
-    def __init__(self, master, props):
-        self.props = props
+    def __init__(self, master, props_iterator):
+        self.props = props_iterator
+        self.current_proposition = None
 
-        frame = Frame(master)
+        self.master = master
+        self.master.title(MAIN_WINDOW_TITLE)
+        self.master.minsize(320, 0)
+
+        frame = Frame(self.master)
         frame.pack()
 
-        self.label_var = StringVar(value=" ")
-        self.label = Label(frame, textvariable=self.label_var)
-        self.label.pack(anchor=CENTER)
+        self.label_var = StringVar(value=LABEL_DEFAULT_TEXT)
+        self.label = Label(frame, textvariable=self.label_var, font=tkFont.Font(size=14))
+        self.label.grid(row=0, column=1, columnspan=3)
 
-        self.scale_var = IntVar()
-        self.scale = Scale(frame, from_=0, to=7, orient="horizontal", variable=self.scale_var)
-        self.scale.pack(anchor=CENTER)
+        self.scale_var = IntVar(value=SCALER_DEFAULT_VALUE)
 
-        self.button = Button(frame, text="Ok!", command=self.next_scale)
-        self.button.pack(side=RIGHT)
+        self.scale = Scale(frame, from_=0, to=7, orient="horizontal", variable=self.scale_var, state=DISABLED, length=300)
+        self.scale.grid(row=1, column=1, columnspan=3)
+
+        self.button = Button(frame, text=BUTTON_START_TEXT, command=self.next_scale)
+        self.button.grid(row=2, column=2)
 
     def next_scale(self):
-        print "new value is", self.scale_var.get()
-        self.label_var.set(self.scale_var.get())
+        try:
+            if self.current_proposition:
+                write_out_answer(self.current_proposition, self.scale_var.get())
+            self.current_proposition = self.props.next()
+            self.label_var.set(proposition_text_view(self.current_proposition))
+            self.scale_var.set(SCALER_DEFAULT_VALUE)
+            self.button['text'] = BUTTON_NEXT_TEXT
+            self.scale['state'] = NORMAL
+        except StopIteration:
+            tkMessageBox.showinfo("", FINISH_MESSAGE)
+            self.master.destroy()
+
+
+def center_window(tk_root):
+    tk_root.update_idletasks()
+    width = tk_root.winfo_width()
+    height = tk_root.winfo_height()
+    x = (tk_root.winfo_screenwidth() / 2) - (width / 2)
+    y = (tk_root.winfo_screenheight() / 2) - (height / 2)
+    tk_root.geometry('{0}x{1}+{2}+{3}'.format(width, height, x, y))
 
 
 if __name__ == "__main__":
     root = Tk()
-    app = ScalerApp(root, generate_stimuli())
+    app = ScalerApp(root, generate_and_shuffle_propositions_generator())
+    center_window(root)
     root.mainloop()
