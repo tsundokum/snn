d12848558de553264567e128fb254bdcbf992d0b
Ilya Pershin <ven_9191@bk.ru>
2013-05-12 23:43:42 +0300

    Added weights history (theta) Corrected history of errors (J)

--- NN_learning.py
+++ NN_learning.py
@@ -25,10 +25,10 @@ hidden_2 = [10]  # Structure of the second subnetwork
 epsilon = 0.2  # Limitation of  initial eights
 alpha = 0.1  # Learning rate
 R = 0.2  # Coefficient of regularization
-M = 0.1  # Moment
+M = 0.0  # Moment
 e = 1e-4  # value of weights changing in the gradient check function
-epochs_count = 100
-batches_count = 1
+epochs_count = 15
+batches_count = 20
 file = 'resources/test-distance-matrix.csv'
 
 
@@ -53,8 +53,12 @@ relation_in_size = np.size(input_relation, 1)  # Relations number
 output_size = np.size(Y, 1)  # Number of attributes
 num_lay_1 = len(hidden_1)           # Number of layers in the first subnetwork
 num_lay_2 = len(hidden_2)           # Number of layers in the second subnetwork
-J = np.zeros((epochs_count, batches_count))
-# theta_history =
+J = range(batches_count * epochs_count + 1)
+# Create structure to store theta matrixes
+theta_history = range(batches_count * epochs_count + 1)
+for i in range(batches_count * epochs_count + 1):
+    theta_history[i] = range(3)
+
 # Data division (optional):
 # ...
 
@@ -70,6 +74,8 @@ J = np.zeros((epochs_count, batches_count))
 #  Create 3 sets of matrices of initial weights according to the given structure.
 [theta_1, theta_2, theta_relation] = neural_network.initialise_weights(input_size, hidden_1, hidden_2, relation_in_size,
                                                                        output_size, num_lay_1, num_lay_2, epsilon)
+# Save original theta matrices
+theta_history[0] = [theta_1, theta_2, theta_relation]
 
 #  Create initial moment for every weight
 [moment_1, moment_2, moment_relation] = neural_network.initialize_moment(theta_1,theta_2,theta_relation)
@@ -84,7 +90,7 @@ for epoch in range(epochs_count):  # Beginning of epoch loop
                                                         X, input_relation, theta_1, theta_2, theta_relation)
 
         # Compute average error with regularization.
-        J[epoch, batch] = neural_network.compute_cost_function(m, a_2, theta_1, theta_2, theta_relation,
+        J[epoch * batches_count + batch] = neural_network.compute_cost_function(m, a_2, theta_1, theta_2, theta_relation,
                                                                num_lay_1, num_lay_2, R, Y)
 
         # Compute derivative of the cost function with respect to matrices theta.
@@ -104,17 +110,30 @@ for epoch in range(epochs_count):  # Beginning of epoch loop
                                                                                    moment_2, moment_relation, M)
 
 
-        """# Save weights values
-        theta_1_temp = theta_history[epoch, batch][0]
-        theta_2_temp = theta_history[epoch, batch][1]
-        theta_relation_temp = theta_history[epoch, batch][2]"""
+        # Save weights values
+        theta_history[epoch * batches_count + batch + 1] = [theta_1_temp,
+                                                            theta_2_temp,
+                                                            theta_relation_temp]
 
         # Update current weight matrices
         theta_1 = theta_1_temp
         theta_relation = theta_relation_temp
         theta_2 = theta_2_temp
 
-print "Errors by epochs = %s" % J.sum(1)
+# Compute final error after all loops of learning
+[a_1, a_2] = neural_network.forward_propagation(m, num_lay_1, num_lay_2, X,
+                                                input_relation, theta_1, theta_2, theta_relation)
+
+J[-1] = neural_network.compute_cost_function(m, a_2, theta_1, theta_2,
+                                                       theta_relation, num_lay_1, num_lay_2, R, Y)
+
+
+# Save theta_log
+# save J
+
+print J
+
+print "Errors by epochs = %s" % J
 """
 # Gradient verification
 [numgrad_1, numgrad_2, numgrad_rel] = neural_network.gradient_check(e, m, X, Y,
