6f1d14bcc148baaf24e154cab874bd836d83426c
Ilya Pershin <ven_9191@bk.ru>
2014-11-02 13:43:51 +0400

    separate functions are formed

--- ERP_processing.py
+++ ERP_processing.py
@@ -1,5 +1,8 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
+import csv
+import glob
 import numpy as np
 import pandas as pd
 import scipy
@@ -18,43 +21,82 @@ def repl_dots(f1, f2):
     f1.close()
     f2.close()
 
-# load list of stimulus types
-st_types = pd.read_csv("c:\\SNN\\ERP_processing\\st_types.csv", header=None, encoding='cp1251').fillna('')
-st_conc = range(len(st_types))
-for i, r in st_types.iterrows():
-    st_conc[i] = [r[0] + ' ' + r[1] + ' ' + r[2], r[3]] # concatenate triads
-st_conc = pd.DataFrame(st_conc)
+def conc_types(st_types_file):
+    """load list of stimulus types"""
+    st_types = pd.read_csv(st_types_file, header=None, encoding='cp1251').fillna('')
+    st_conc = range(len(st_types))
+    for i, r in st_types.iterrows():
+        st_conc[i] = [r[0] + ' ' + r[1] + ' ' + r[2], r[3]] # concatenate triads
+    return pd.DataFrame(st_conc)
 
-# recognize types of triads
-path_dist = "c:\\SNN\\ERP_processing\\distances\\"
-names = []
-types = []
-dists = []
-for dist_file in os.listdir(path_dist):
-    distances = pd.read_csv(path_dist + dist_file, header=None, encoding='utf-8').fillna('')
-    for i, r in distances.iterrows():
-        pos = st_conc[0].str.contains(r[0] + ' ' + r[1] + ' ' + r[2]) # find this triad
-        n_type = int(st_conc[1][pos]) - 128
-        names.append(dist_file[0:dist_file.find(' ')])
-        types.append(n_type)
-        dists.append(int(r[3]))
-        print i
+st_types_file = "c:\\SNN\\epr_data_by_10_14\\preprocessing\\st_types.csv"
+st_conc = conc_types(st_types_file)
 
-estimated_triads = pd.DataFrame({'names': pd.Series(names),
-                                 'types': np.array(types),
-                                 'distances': np.array(dists)},
-                                 columns=['names', 'types', 'distances'])
-estimated_triads.to_csv("c:\\SNN\\ERP_processing\\estimated_triads.csv", index=False)
+# version for infotable (codes, real names, types of stimuli) using
+def recognize_types_info(path_dist, infotable):
+    """recognize types of triads"""
+    names = []
+    types = []
+    dists = []
+    for dist_file in os.listdir(path_dist):
+        if dist_file.endswith('.csv'):
+            print dist_file
+            distances = pd.read_csv(path_dist + dist_file, header=None, encoding='utf-8').fillna('')
+            for i, r in distances.iterrows():
+                pos = st_conc[0].str.contains(r[0] + ' ' + r[1] + ' ' + r[2]) # find this triad
+                n_type = int(st_conc[1][pos]) - 128
+                name = infotable.Name[infotable.Code == dist_file[0:dist_file.find('_')]]
+                names.append(name.values[0])
+                types.append(n_type)
+                dists.append(int(r[3]))
+    estimated_triads = pd.DataFrame({'names': pd.Series(names),
+                                     'types': np.array(types),
+                                     'distances': np.array(dists)},
+                                     columns=['names', 'types', 'distances'])
+    return estimated_triads
 
-estimated_triads = pd.read_csv("c:\\SNN\\ERP_processing\\estimated_triads.csv")
+# read info table
+infotable = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\preprocessing\\infoTable.csv", sep=',', encoding='cp1251')
+path_dist = 'c:\\SNN\\epr_data_by_10_14\\dist_renamed\\'
+estimated_triads = recognize_types_info(path_dist, infotable)
+estimated_triads.to_csv("c:\\SNN\\epr_data_by_10_14\\preprocessing\\estimated_triads.csv", index=False, encoding='cp1251')
+estimated_triads = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\preprocessing\\estimated_triads.csv", encoding='cp1251')
+
+# version for proper file names
+def recognize_types(path_dist):
+    """recognize types of triads"""
+    names = []
+    types = []
+    dists = []
+    for dist_file in os.listdir(path_dist):
+        if dist_file.endswith('.csv'):
+            print dist_file
+        distances = pd.read_csv(path_dist + dist_file, header=None, encoding='utf-8').fillna('')
+        for i, r in distances.iterrows():
+            pos = st_conc[0].str.contains(r[0] + ' ' + r[1] + ' ' + r[2]) # find this triad
+            n_type = int(st_conc[1][pos]) - 128
+            names.append(dist_file[0:dist_file.rfind(' ')])
+            types.append(n_type)
+            dists.append(int(r[3]))
+    estimated_triads = pd.DataFrame({'names': pd.Series(names),
+                                     'types': np.array(types),
+                                     'distances': np.array(dists)},
+                                     columns=['names', 'types', 'distances'])
+    return estimated_triads
+
+path_dist = 'c:\\SNN\\epr_data_by_10_14\\dist_renamed\\'
+estimated_triads = recognize_types(path_dist)
+estimated_triads.to_csv("c:\\SNN\\epr_data_by_10_14\\preprocessing\\estimated_triads.csv", index=False, encoding='cp1251')
+estimated_triads = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\preprocessing\\estimated_triads.csv", encoding='cp1251')
 
 
-# read erp data excluding bio and pg channels
-erps = pd.read_csv("c:\\SNN\\ERP_processing\\ERP_result.csv", sep=';', decimal=',').ix[:,:-81]
+# read erp data excluding bio and pg channels (1000, 0, 50)
+erps = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\preprocessing\\result.csv", \
+                   sep=';', decimal=',', encoding='cp1251').ix[:,:-81]
 
-
-# get unique names and types of stimulus
-types = np.sort(erps.TypStim.unique())
+# read erp data excluding bio and pg channels (1100, -100, 50)
+erps = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\preprocessing\\result(1100,-100,50).csv", \
+                   sep=';', decimal=',', encoding='cp1251').ix[:,:-41]
 
 
 def exclude_prob(erps, name_num):
@@ -63,12 +105,30 @@ def exclude_prob(erps, name_num):
     print 'excluding '+names[name_num]
     erps = erps[~(erps.Name == names[name_num])]
     erps.to_csv("c:\\SNN\\ERP_processing\\ERPdata\\erp.csv", index=False, sep=';')
-    repl_dots("c:\\SNN\\ERP_processing\\ERPdata\\erp_avg_excl.csv", "c:\\SNN\\ERP_processing\\ERPdata\\erp_avg_excl(,).csv")
+    repl_dots("c:\\SNN\\ERP_processing\\ERPdata\\erp_avg_excl.csv", \
+              "c:\\SNN\\ERP_processing\\ERPdata\\erp_avg_excl(,).csv")
     names = erps.Name.unique()
     for n in names: print n
     return erps
 
+##test1 = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\result1.csv", sep=';', decimal=',')
+##test2 = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\result2.csv", sep=';', decimal=',')
+####import matplotlib.pyplot as pp
+##test_f1 = np.array(test1.ix[0,4:-2], dtype='float')
+##test_f2 = np.array(test2.ix[0,4:-2], dtype='float')
+##print test_f1.shape, test_f2.shape
+##
+##disp_learning_dynamic(test_f1, test_f2)
+##
 
+
+nchan = 19
+nstep = 22
+##def ProcessERP(nchan, nstep, erps, subtr_mean):
+##    """..."""
+# get unique names and types of stimulus
+types = np.sort(erps.TypStim.unique())
+names = erps.Name.unique()
 #  add real distances (averaging)
 erp_processed = pd.DataFrame()
 for n in names:
@@ -76,21 +136,58 @@ for n in names:
     erp_n = erps[erps.Name == n]
     dist_by_name = estimated_triads[estimated_triads.names == n]
     for t in types:
-       print t
-       erp_n_t = erp_n.ix[erp_n.TypStim == t, 4:]
-       dist = int(dist_by_name.distances[dist_by_name.types == t])
-##       avg_erp = pd.DataFrame(np.mean(erp_n_t)).T  # averaging
-       avg_erp = pd.DataFrame(erp_n_t)  # withount averaging
-       avg_erp['Name'] = pd.Series(n, index=avg_erp.index)
-       avg_erp['TypStim'] = pd.Series(t, index=avg_erp.index)
-       avg_erp['Dist'] = pd.Series(dist, index=avg_erp.index)
-       erp_processed = pd.concat((erp_processed, avg_erp))
+        print t
+        erp_n_t = erp_n.ix[erp_n.TypStim == t, 4:]
+        dist = int(dist_by_name.distances[dist_by_name.types == t])
+        processed_n_t = pd.DataFrame()
+        # subtract average on every channel
+        for ch in range(nchan):
+            erp_n_t_ch = erp_n_t.iloc[:,nstep*ch:((ch+1)*nstep)]  # components per cahnnel
+            subtrahend = erp_n_t_ch.iloc[:,:2].mean(1)  # take an average of two previous components
+            differ = erp_n_t_ch.iloc[:,2:].subtract(subtrahend, axis=0)  # subtraction
+            processed_n_t = pd.concat([processed_n_t, differ], axis=1)
+        ##       avg_erp = pd.DataFrame(np.mean(erp_n_t)).T  # averaging
+##        avg_erp = pd.DataFrame(processed_n_t)  # withount averaging
+        avg_erp = processed_n_t  # withount averaging
+        avg_erp['Name'] = pd.Series(n, index=avg_erp.index)
+        avg_erp['TypStim'] = pd.Series(t, index=avg_erp.index)
+        avg_erp['Dist'] = pd.Series(dist, index=avg_erp.index)
+        erp_processed = pd.concat((erp_processed, avg_erp))
 # replace columns
 cols = ['Name'] + ['TypStim'] + ['Dist'] + list(erps.columns[4:])
 erp_processed_ord = erp_processed[cols]
 erp_processed_ord.to_csv("c:\\SNN\\ERP_processing\\ERPdata\\erp_dist.csv", index=False, decimal=',', sep=';')
 erps = pd.read_csv("c:\\SNN\\ERP_processing\\ERPdata\\erp_dist.csv", decimal='.', sep=';')
 repl_dots("c:\\SNN\\ERP_processing\\ERPdata\\erp_dist.csv", "c:\\SNN\\ERP_processing\\ERPdata\\erp_dist(,).csv")
+
+# JUNK
+for n in names:
+    if sum(erps.Name == n) < 300:
+        print n
+
+
+
+# WITHOUT AVERAGING
+# get unique names and types of stimulus
+types = np.sort(erps.TypStim.unique())
+names = erps.Name.unique()
+#  add real distances (averaging)
+erp_processed = pd.DataFrame()
+for n in names:
+    print n
+    erp_n = erps[erps.Name == n]
+    dist_by_name = estimated_triads[estimated_triads.names == n]
+    for t in types:
+        print t
+        erp_n_t = erp_n.ix[erp_n.TypStim == t, 4:]
+        dist = int(dist_by_name.distances[dist_by_name.types == t])
+        ##       avg_erp = pd.DataFrame(np.mean(erp_n_t)).T  # averaging
+        avg_erp = pd.DataFrame(erp_n_t)  # withount averaging
+        avg_erp['Name'] = pd.Series(n, index=avg_erp.index)
+        avg_erp['TypStim'] = pd.Series(t, index=avg_erp.index)
+        avg_erp['Dist'] = pd.Series(dist, index=avg_erp.index)
+        erp_processed = pd.concat((erp_processed, avg_erp))
+
 
 
 # add group and known\unknown type
@@ -307,6 +404,33 @@ for file_name in os.listdir(direct):
     print file_name
     dist = pd.read_csv(direct+file_name, header=None, encoding='utf-8')
     print dist
+
+
+estimated_triads = pd.read_csv("c:\\SNN\\ERP_processing\\estimated_triads.csv", encoding='cp1251')
+stimulus = pd.read_csv("c:\\SNN\\epr_data_by_10_14\\stimuls.csv", encoding='cp1251', header=None)
+
+names = estimated_triads.names.unique()
+for n in names:
+    type_dist = estimated_triads.ix[estimated_triads.names == n,[1,2]]
+##    round_dist = (dist / 2.5).round()
+##    type_dist.set_index([range(len(type_dist))])
+    type_dist.index = range(len(type_dist))
+    dist = type_dist.distances
+    round_dist = type_dist.distances.copy()
+    round_dist[dist>5] = 2
+    round_dist[dist==0] = 0
+    round_dist[(dist>0) & (dist<=5)] = 1
+    type_dist.distances = round_dist
+    dist_bystim = range(len(stimulus))
+    for i in range(len(stimulus)):
+        dist_bystim[i] = int(type_dist.ix[type_dist.types == stimulus.ix[i,0], 1])
+
+    filename = "c:\\SNN\\ERP_processing\\dist_by_stim\\" + n +".txt"
+    with open(filename, 'wb') as f:
+        writer = csv.writer(f)
+        for val in dist_bystim:
+            writer.writerow([val])
+    print filename
 
 
 
